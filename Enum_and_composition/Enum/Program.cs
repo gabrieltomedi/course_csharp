382b56c31f44e5c9d97c0a6e646e6b56a340fae6
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-05-09 20:17:44 -0300

    Enum Exemple

--- Enum_and_composition/Enum/Program.cs
+++ Enum_and_composition/Enum/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EnumExemple.Entities;
+using EnumExemple.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExemple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1293,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);  
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            /*
+              Outras formas
+              OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");
+
+              OrderStatus os;
+              Enum.TryParse("Delivered", out os);  
+             */
+
+
+            Console.WriteLine(txt); 
+            Console.WriteLine(os);
+
+            
+
+        }
+    }
+}
