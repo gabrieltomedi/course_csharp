44a1ab117d3e828c380a2772772be1d1987ed8ad
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-05-18 19:01:16 -0300

    Created board and class position

--- ChessGame_Console/ChessGame_Console/Program.cs
+++ ChessGame_Console/ChessGame_Console/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace ChessGame_Console
+﻿using Board;
+
+namespace ChessGame_Console
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            Position p = new Position(3, 4);
 
+            Console.Write("Position: " + p);
+            Console.WriteLine();
         }
     }
 }
