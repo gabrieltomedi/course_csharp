bda8a4b9c1adbef4ed9af90917712d6016508d9e
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-05-26 14:22:24 -0300

    Desmostration of StreamWriter

---
+++ Working_With_Files/Files5_StreamWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace Files5_StreamWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\gabri\OneDrive\Área de Trabalho\Udemy\C# Completo\Seção 13 - Trabalhando com arquivos\File1.txt";
+            string targetPath = @"C:\Users\gabri\OneDrive\Área de Trabalho\Udemy\C# Completo\Seção 13 - Trabalhando com arquivos\File3.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw= File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error Ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
