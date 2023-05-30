2a6b7aa8fdbc40d0cb7b5538ec30664b4f2ce722
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-05-30 19:24:18 -0300

    Desmostration of Directory and DirectoryInfo

---
+++ Working_With_Files/Files6_Directory/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace Files6_Directory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\temp\myfolder";
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Folders: ");
+                foreach (string folder in folders)
+                {
+                    Console.WriteLine(folder);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Files: ");
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+
+                Directory.CreateDirectory(path + "\\newfolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
