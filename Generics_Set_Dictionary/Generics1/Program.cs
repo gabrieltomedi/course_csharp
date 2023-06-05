8f726a57a2da750aa9336d66e859bda3f5d623df
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-06-05 15:30:20 -0300

    Demonstration of Generics 1

---
+++ Generics_Set_Dictionary/Generics1/Program.cs
@@ -0,0 +1,39 @@
+﻿using Generics1.Services;
+
+namespace Generics1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());  
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValues(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.GetFirst());
+
+            Console.WriteLine("\n-----string-----\n");
+            PrintService<string> printService2 = new PrintService<string>();
+
+            Console.Write("How many values? ");
+             n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string x = Console.ReadLine();
+                printService2.AddValues(x);
+            }
+
+            printService2.Print();
+            Console.WriteLine("First: " + printService2.GetFirst());
+        }
+    }
+}
---
+++ Generics_Set_Dictionary/Generics1/Services/PrintService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics1.Services
+{
+    internal class PrintService<Type>
+    {
+        private Type[] _values = new Type[10];
+        private int _count = 0;
+
+        public void AddValues(Type values)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrintServicec is full");
+            }
+            _values[_count] = values;
+            _count++;
+        }
+
+        public Type GetFirst() 
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintServicec is empty");
+            }
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if(_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
