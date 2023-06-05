fb0e80a6834bcaaba7417c4a950e1a52cfb75af2
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-06-05 14:38:50 -0300

    Demonstration of Interface IComparable

---
+++ Interfaces/Interface4_IComparable/Program.cs
@@ -0,0 +1,34 @@
+﻿using Interface4_IComparable.Entities;
+
+namespace Interface4_IComparable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\temp\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));    
+                    }
+                    list.Sort();
+                    foreach (Employee employee in list)
+                    {
+                        Console.WriteLine(employee);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(ex.Message);   
+            }
+        }
+    }
+}
