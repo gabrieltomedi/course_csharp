7076a871b939c95742d6a684e85aa16e196b90ef
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-06-09 15:01:34 -0300

    Exemple of HashSet

---
+++ Generics_Set_Dictionary/HashSetAndSortedSet1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace HashSetAndSortedSet1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+            Console.WriteLine(set.Contains("Computer"));
+
+            foreach(string s in set)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
