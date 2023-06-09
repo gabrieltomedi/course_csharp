72dd9732753972b4f2c8f1c7b46a421095fab926
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-06-09 14:04:52 -0300

    Demonstration of generics 2

---
+++ Generics_Set_Dictionary/Generics2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+using Generics2.Services;
+using Generics2.Entities;
+
+namespace Generics2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            Console.WriteLine("---Integer---");
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            int max = calculationService.Max(list);
+
+            Console.Write("Max: ");
+            Console.WriteLine(max);
+
+            List<Product> list2 = new List<Product>();
+
+            Console.WriteLine("---Products---");
+            Console.Write("Enter N: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+
+                list2.Add(new Product(name, price));
+            }            
+
+            Product max2 = calculationService.Max(list2);
+
+            Console.Write("Max: ");
+            Console.WriteLine(max2);
+        }
+    }
+}
