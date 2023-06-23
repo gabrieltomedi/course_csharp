535705b1f027b6ef13efa0947e0657ddc6bc28e3
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-06-23 17:40:56 -0300

    demonstration of Extension Methods 1

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExtensionMethods.Extensions;
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2023, 6, 23, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
