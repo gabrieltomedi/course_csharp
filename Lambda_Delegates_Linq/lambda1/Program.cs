63ee02661954c051678095ab177369e5e0894071
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-06-27 18:38:53 -0300

    Inline Lambda Expression

--- Lambda_Delegates_Linq/lambda1/Program.cs
+++ Lambda_Delegates_Linq/lambda1/Program.cs
@@ -12,11 +12,13 @@ namespace lambda1
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Table", 450.00));
 
-            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+            /*
+             Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
              
-            list.Sort(comp);
-            
-            //list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+             list.Sort(comp);
+            */
+
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
 
             foreach (Product p in list)
             {
