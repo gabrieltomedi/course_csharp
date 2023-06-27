0d526752aaf1f727c3ada9c8d65b9e40935f524c
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-06-27 18:28:33 -0300

    Demonstration of Delegate

---
+++ Lambda_Delegates_Linq/lambda1/Program.cs
@@ -0,0 +1,33 @@
+﻿using lambda1.Entities;
+
+namespace lambda1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Table", 450.00));
+
+            /*
+             * Comparison<Product> comp = CompareProduct
+             * list.Sort(comp);
+             */
+
+            list.Sort(CompareProducts);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        static int CompareProducts(Product p1, Product p2)
+        {
+            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+        }
+    }
+}
