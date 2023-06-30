a1d433796c0d2aedf17ae907d3de4b73e65cc6ce
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-06-30 16:42:52 -0300

    Demonstration of how to delcare a delegate

---
+++ Lambda_Delegates_Linq/delegate1/Program.cs
@@ -0,0 +1,26 @@
+﻿using delegate1.Services;
+
+namespace delegate1
+{
+    internal class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op =  CalculationService.Sum;
+            // BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+
+            double result = CalculationService.Sum(a, b);
+            Console.WriteLine(result);
+
+
+            //result = op.Invoke(a, b);
+            result = op(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
