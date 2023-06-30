3e2215af87ff0dd37632c606de24f2a5757c66d1
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-06-30 16:50:25 -0300

    Demonstration of multcast delegate

---
+++ Lambda_Delegates_Linq/delegate2_multicast/Program.cs
@@ -0,0 +1,20 @@
+﻿using delegate2_multicast.Services;
+
+namespace delegate2_multicast
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+            
+            // op.Invoke(a, b);
+            op(a, b);
+        }
+    }
+}
